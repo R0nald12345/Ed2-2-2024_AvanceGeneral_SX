b425ebe8b715bf9dfc29fb664381a35579474325
= <pumaronal2016@gmail.com>
2024-09-12 14:52:50 -0400

    Se agrego nuevos ejercicios

--- src/PrimerParcial/ListaArreglo/ClaseListaArreglo.java
+++ src/PrimerParcial/ListaArreglo/ClaseListaArreglo.java
@@ -95,8 +95,95 @@ public class ClaseListaArreglo {
                     return cantidad;
                 }
         }
-                
     }
+    
+    public void insertarNum(int num, int pos){
+        insertarNumRecursivo(this.cant, num, pos);
+        this.cant++;
+    }
+    
+    private void insertarNumRecursivo(int cantAux,int num,int pos){
+        if(cantAux == pos){
+            vector[cantAux] = num;
+//            this.cant++;
+        }else{
+//            insertarNumRecursivo(cantAux-1, num, pos );
+//            if(cantAux == pos ){
+//                vector[cantAux] = num;
+////                this.cant++;
+//            }
+//            
+            vector[cantAux] = vector[cantAux - 1];  // Desplaza el elemento hacia adelante
+        insertarNumRecursivo(cantAux - 1, num, pos);  // Llamada recursiva
+        }
+    }
+    
+    public boolean verificarTodoIgual(){
+        return verificarTodoIgualRecursivo(this.cant);
+    }
+    
+    private boolean verificarTodoIgualRecursivo(int cantAux){
+        //Caso Base
+        if(cantAux == 1){
+            return true;
+        }else{ //caso General
+            boolean res = verificarTodoIgualRecursivo(cantAux-1);
+            int datoAnterior= vector[cantAux-2]; // 6
+            if(datoAnterior == vector[cantAux-1]){ // 6 == 1
+                res =  true;
+            }else{
+                res = false;
+            }
+            return res;
+//            return datoAnterior == vector[cantAux-1];
+        }
+    } 
+    
+    public int frecuenciaNumero(int x){
+        return frecuenciaNumeroRecursivo(this.cant, x);
+    }
+    
+    private int frecuenciaNumeroRecursivo(int cantAux, int x){
+        //caso Base
+        if(cantAux == 1 ){
+            if(vector[cantAux -1] == x){
+                return 1;
+            }else{
+                return 0;
+            }
+        }else{ // caso General
+            int cantidad = frecuenciaNumeroRecursivo(cantAux-1,x);
+            if(vector[cantAux-1] == x){ //verifico ultima posicion
+                cantidad++;
+            }
+            return cantidad;
+        }
+    }
+    
+    public int sumarDigitoPar(){
+        return sumarDigitoParRecursivo(this.cant);
+    }
+    
+    private int sumarDigitoParRecursivo(int cantAux ){
+        if(cantAux == 1){
+            if((this.vector[cantAux-1] %2) == 0){
+                return vector[cantAux-1];
+            }else{
+                return 0;
+            }
+        }else{ //Caso General
+            int sumaTotal =sumarDigitoParRecursivo(cantAux - 1); 
+            //trato de validar el ultimo dato del Vector
+            if((this.vector[cantAux-1] % 2) == 0){
+                return sumaTotal + vector[cantAux-1];
+            }else{
+                return sumaTotal;
+            }
+        }
+    }
+    
+    
+    
     
     
     public int contarNumerosImpares() {
--- src/PrimerParcial/ListaArreglo/Main.java
+++ src/PrimerParcial/ListaArreglo/Main.java
@@ -15,17 +15,26 @@ public class Main {
         
         ClaseListaArreglo l1 = new ClaseListaArreglo();
         
-        l1.insertarUlt(3);
-        l1.insertarUlt(4);
-        l1.insertarUlt(6);
-        l1.insertarUlt(9);
-        l1.insertarUlt(2);
+        l1.insertarUlt(1);
+        l1.insertarUlt(1);
+        l1.insertarUlt(1);
+        l1.insertarUlt(1);
+        l1.insertarUlt(1);
         l1.insertarUlt(1);
         
         System.out.println(l1.mostrarLista());
-        System.out.println(l1.obtenerMenorR());
-        System.out.println(l1.obtenerMayorR());
-        System.out.println("Contar Par: " + l1.contarParesR());
+//        System.out.println(l1.obtenerMenorR());
+//        System.out.println(l1.obtenerMayorR());
+//        System.out.println("Contar Par: " + l1.contarParesR());
+
+//        l1.insertarNum( 1000, 2);
+//        System.out.println(l1.mostrarLista());
+
+//            System.out.println( "contar Numeros Pares:" + l1.sumarDigitoPar());
+
+//            System.out.println( "Frecuencia 1:" + l1.frecuenciaNumero(1));
+            System.out.println( "Verificar Iguales:" + l1.verificarTodoIgual());
+            
     }
     
 }
